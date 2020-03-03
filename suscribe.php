3fcf1c70629d6214e8f200dfdb03398e58347d84
i339972 <cesar.estrada@sap.com>
2020-03-03 00:03:58 -0500

    Primer commit

---
+++ suscribe.php
@@ -0,0 +1,35 @@
+<?php
+$to="your@email.com";
+/*Your Email*/
+$subject="Suscription from the website";
+/*Issue*/
+$date=date("l, F jS, Y");
+$time=date("h:i A");
+
+$email  = $_REQUEST['email'];
+$name   = $_REQUEST['name'];
+$course = $_REQUEST['course'];
+
+
+$msg="
+	
+	Suscription request from website on date  $date, hour: $time.\n
+	
+	name: $name\n
+	Email: $email\n
+	course: $course\n
+	
+	";
+if($email=="") {
+echo "<div class='alert alert-danger'>
+		  <a class='close' data-dismiss='alert'>×</a>
+		  <strong>Warning!</strong> Please fill all the fields.
+	  </div>";
+} else {
+mail($to,$subject,$msg,"From:".$email);
+echo "<div class='alert alert-success'>
+		  <a class='close' data-dismiss='alert'>×</a>
+		  <strong>Thank you for your message!</strong>
+	  </div>";
+}
+?>
