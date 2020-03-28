05c4f8e39fe378b47edc3b329d2afecbcbf1ea48
cesar.estrada <cesar.estrada@sap.com>
2020-03-28 17:57:17 -0500

    Move between registro and landing page

--- solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/MainActivity.java
+++ solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/MainActivity.java
@@ -2,7 +2,9 @@ package me.soluciona.clasesonline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
     }
 }
--- solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/Registro.java
+++ solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/Registro.java
@@ -6,11 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ProfileActivity extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.landing_page);
+        setContentView(R.layout.activity_registro);
     }
 }
