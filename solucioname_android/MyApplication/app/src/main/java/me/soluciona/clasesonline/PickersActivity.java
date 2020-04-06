309f2b943ab819922b0e70eeb8c84a650035f5aa
renato.davila.malpica <renato.davila.malpica@gmail.com>
2020-04-06 09:16:44 -0500

    tmr

--- solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/PickersActivity.java
+++ solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/PickersActivity.java
@@ -1,7 +1,5 @@
 package me.soluciona.clasesonline;
-
-import android.annotation.SuppressLint;
-import android.app.DatePickerDialog;
+import android.app.DatePickerDialog;;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +8,20 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import java.util.Calendar;
-@SuppressLint("Registered")
+
 public class PickersActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String CERO = "0";
     private static final String BARRA = "/";
 
+    //Calendario para obtener fecha & hora
     public final Calendar c = Calendar.getInstance();
 
+    //Variables para obtener la fecha
     final int mes = c.get(Calendar.MONTH);
     final int dia = c.get(Calendar.DAY_OF_MONTH);
     final int anio = c.get(Calendar.YEAR);
 
+    //Widgets
     EditText etFecha;
     ImageButton ibObtenerFecha;
 
@@ -28,33 +29,45 @@ public class PickersActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        etFecha = findViewById(R.id.et_mostrar_fecha_picker);
-        ibObtenerFecha = findViewById(R.id.ib_obtener_fecha);
+        //Widget EditText donde se mostrara la fecha obtenida
+        etFecha = (EditText) findViewById(R.id.et_mostrar_fecha_picker);
+        //Widget ImageButton del cual usaremos el evento clic para obtener la fecha
+        ibObtenerFecha = (ImageButton) findViewById(R.id.ib_obtener_fecha);
+        //Evento setOnClickListener - clic
         ibObtenerFecha.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.ib_obtener_fecha) {
-            obtenerFecha();
+        switch (v.getId()){
+            case R.id.ib_obtener_fecha:
+                obtenerFecha();
+                break;
         }
     }
 
     private void obtenerFecha(){
         DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @SuppressLint("SetTextI18n")
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
                 final int mesActual = month + 1;
-                String diaFormateado = (dayOfMonth < 10)? CERO + dayOfMonth :String.valueOf(dayOfMonth);
-                String mesFormateado = (mesActual < 10)? CERO + mesActual :String.valueOf(mesActual);
+                //Formateo el día obtenido: antepone el 0 si son menores de 10
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                //Formateo el mes obtenido: antepone el 0 si son menores de 10
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                //Muestro la fecha con el formato deseado
                 etFecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
 
 
             }
-
+            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+            /**
+             *También puede cargar los valores que usted desee
+             */
         },anio, mes, dia);
+        //Muestro el widget
         recogerFecha.show();
 
     }
