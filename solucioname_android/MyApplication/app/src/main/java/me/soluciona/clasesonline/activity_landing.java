51b067e3014b8296cb2307160505e22cfdfffa42
cesar.estrada <cesar.estrada@sap.com>
2020-04-08 21:03:41 -0500

    Fix for main ACtivity landing

---
+++ solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/activity_landing.java
@@ -0,0 +1,14 @@
+package me.soluciona.clasesonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
