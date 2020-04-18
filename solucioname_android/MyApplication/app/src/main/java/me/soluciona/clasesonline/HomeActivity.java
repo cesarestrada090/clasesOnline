b1c94da85cd8d415fe09e76c4519b14d3bd2c28a
renato.davila.malpica <renato.davila.malpica@gmail.com>
2020-04-18 09:32:49 -0500

    mm

--- solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/HomeActivity.java
+++ solucioname_android/MyApplication/app/src/main/java/me/soluciona/clasesonline/HomeActivity.java
@@ -1,9 +1,13 @@
 package me.soluciona.clasesonline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,16 +16,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+
 public class HomeActivity extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
     private Button btnSignOut;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnSignOut = findViewById(R.id.btnLogOut);
+
+
 
         // Google Authenticator State
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
